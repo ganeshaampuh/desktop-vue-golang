607076b04bcdc7e05316b76c47213a50ea645262
Ganesha Ampuh <ganesha.ampuh@mekari.com>
2023-01-04 14:36:47 +0700

    remove handlers

--- src/preload.js
+++ src/preload.js
@@ -7,13 +7,14 @@ const path = require('path')
 function loadBackend() {
     var { exec } = require('child_process');
     var executablePath = path.join(__dirname, 'backend/crud');
-    exec(executablePath, function (err, data) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log(data.toString());
-    });
+    exec(executablePath);
+    // exec(executablePath, function (err, data) {
+    //     if (err) {
+    //         console.error(err);
+    //         return;
+    //     }
+    //     console.log(data.toString());
+    // });
 };
 
 contextBridge.exposeInMainWorld('loadBackend', loadBackend)
